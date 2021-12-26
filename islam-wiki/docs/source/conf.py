6ce7ed964ef8d7b77aa705b3e330f0094fe836b6
fanil-z <68493462+fanil-z@users.noreply.github.com>
2021-12-26 22:32:39 +0100

    made many things today Alhamdulillah

--- islam-wiki/docs/source/conf.py
+++ islam-wiki/docs/source/conf.py
@@ -53,3 +53,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+html_css_files = ['css/custom.css']
